124d2a266addb7c4ca456d96d158e235ed61e972
Aldi Nurhanudin <aldinurhanudin08@gmail.com>
2023-05-28 21:23:54 +0700

    database realtime firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:quiz/models/db_connect.dart';
+import 'package:quiz/models/question_model.dart';
 import 'package:quiz/screens/home_screen.dart';
 
 void main(List<String> args) {
+  var db = DBconnect();
+  db.addQuestion(Question(id: '20', title: 'What is  20 x 100 ?', options: {
+    '100': false,
+    '200': true,
+    '300': false,
+    '500': false,
+  }));
   runApp(
     const MyApp(),
   );
@@ -12,9 +21,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  const MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-
       home: HomeScreen(),
     );
   }
---
+++ lib/models/db_connect.dart
@@ -0,0 +1,16 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:quiz/models/question_model.dart';
+
+class DBconnect {
+  final url = Uri.parse(
+      'https://quiz-8ee28-default-rtdb.firebaseio.com/questions.json');
+  Future<void> addQuestion(Question question) async {
+    http.post(url,
+        body: json.encode({
+          'title': question.title,
+          'options': question.options,
+        }));
+  }
+}
